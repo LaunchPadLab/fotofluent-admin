d0c304ff9177dcd821d77a99755be85f487bc1ec
dianacamacho <dcam1901@gmail.com>
2017-07-13 22:10:00 -0500

    pagination on top and bottom, 10 per page

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,5 @@ class Word < ApplicationRecord
   has_many :languages, through: :translations
   accepts_nested_attributes_for :translations, reject_if: :all_blank, allow_destroy: true
   scope :unsplash, -> { where('image LIKE ?', '%unsplash%') }
+  paginates_per 10
 end
